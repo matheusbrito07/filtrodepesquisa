7576816e152f151e0a6eb5ce8786fddfe84ff81d
Matheus de Lira Brito <950721@corp.casaevideo.com.br>
2021-09-01 10:29:38 -0300

    filtro de pesquisa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const blusas = [
+  {categoria:'tshirt', cor:'branca', tamanho:'M'},
+  {categoria:'tshirt', cor:'preto', tamanho:'M'},
+  {categoria:'casaco', cor:'branca', tamanho:'M'},
+  {categoria:'casaco', cor:'preto', tamanho:'M'},
+  {categoria:'regata', cor:'branca', tamanho:'M'},
+  {categoria:'regata', cor:'preto', tamanho:'M'},
+]
+
+const linhas=(cat)=>{
+  const li=[]
+  blusas.forEach(
+    (blusa)=>{
+      if(blusa.categoria===cat||cat===''){
+        li.push(
+          <tr>
+            <td>{blusa.categoria}</td>
+            <td>{blusa.cor}</td>
+            <td>{blusa.tamanho}</td>
+          </tr>
+        )
+      }
+    }
+  )
+  return li
+}
+
+
+const tabelaBlusas=(cat)=>{
+  return(
+    <table border='1'>
+      <thead>
+        <tr>
+          <th>Categoria</th> 
+          <th>Cor</th> 
+          <th>Tamanho</th> 
+           
+        </tr>
+      </thead>
+      <tbody>
+        {linhas(cat)}
+      </tbody>
+    </table>
+  )
+}
+
+const pesquisaCategoria=(cat,scat)=>{
+  return(
+    <div>
+      <label> Digite a categoria</label>
+      <input type='text' value={cat} onChange={(e)=>scat(e.target.value)}/>
     </div>
-  );
+  )
+}
+
+
+
+function App() {
+
+  const [categoria, setCategoria]=useState('')
+  return (
+    <>
+    {pesquisaCategoria(categoria,setCategoria)}
+    <br/>
+    {tabelaBlusas(categoria)}
+    </>
+  )
 }
 
 export default App;
